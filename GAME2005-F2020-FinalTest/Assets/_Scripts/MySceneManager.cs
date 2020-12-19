f184ff2068b47b95dd8c4dd3ed013c9ebd2cc3c8
Andrew Boulanger <andrew_boulanger14@hotmail.com>
2020-12-18 20:32:39 -0500

    adding initial files, also started making start scene

---
+++ GAME2005-F2020-FinalTest/Assets/_Scripts/MySceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MySceneManager : MonoBehaviour
+{
+
+    public void loadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void loadPlayScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
